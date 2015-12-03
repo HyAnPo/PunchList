ff2728f9813bebd3c121b3b7fcae36eab92eedb8
Andrew Porter <haporter@me.com>
2015-12-03 14:56:00 -0700

    beginning model objects

---
+++ PunchList/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  PunchList
+//
+//  Created by Andrew Porter on 12/3/15.
+//  Copyright © 2015 Andrew Porter. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    
+}
